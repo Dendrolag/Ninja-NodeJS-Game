6b690b722a58efcf91472fbdcbf0bd137bfba274
Dendrolag <project@remibriere.com>
2024-10-25 19:13:36 +0200

    Configurer le serveur pour utiliser process.env.PORT

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const waitingRoom = {
     isGameStarted: false
 };
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Configuration des fichiers statiques
 app.use('/assets', express.static(__dirname + '/assets'));
